97b03863dbdbe1e543b385be2aa08d72982df8af
geno <yvesgeno@outlook.com>
2024-06-29 10:13:15 +0200

    refresh

--- wce/backend/utils/middleware.js
+++ wce/backend/utils/middleware.js
@@ -1,7 +1,7 @@
 
 const logger = require('./logger')
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const User = require('../models/User')
 require('dotenv').config();
 const SECRET = process.env.SECRET
